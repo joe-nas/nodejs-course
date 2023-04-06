41548fdea705e6fdecaf9f8a315ce120f2014374
joe-nas <jonas@falcken.de>
2023-04-06 18:23:40 +0200

    reading and writing files

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,13 @@
-const hello = "hello world";
-console.log(hello);
+// const hello = "hello world";
+// console.log(hello);
 
 const fs = require("fs");
 // const exercises = fs.open("/Users/affennacken/Coding/exercises.json");
+
+// fs.readFileSync("/Users/affennacken/Coding/exercises.json", );
+const textin = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textin);
+
+const textout = `This is what we know about the avocado ${textin}. \n Created on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textout);
+console.log("file has been written");
