cae05865e6a2d3faa0966213c55201f095f0b00a
joe-nas <jonas@falcken.de>
2023-04-11 15:47:04 +0200

    middleware chaining

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -16,6 +16,16 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 400,
+      message: 'Missing name or price!',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   // using our own middleware
   console.log(req.requestTime);
