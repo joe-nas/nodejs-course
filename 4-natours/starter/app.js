0dc982c6d47e6f17aaa62ab883175d81c5c5f762
joe-nas <jonas@falcken.de>
2023-04-11 13:20:15 +0200

    creating own middleware, using it and  learning about positioning

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -6,6 +6,17 @@ const app = express();
 // Middleware: can modify the incoming request data
 app.use(express.json()); // adds the request body to request object, would be undefined otherwise
 
+// own middleware: needs to positioned before route handler in order to being used
+app.use((req, res, next) => {
+  console.log('Hello this is our own middleware 🐶🐶');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const port = 3000;
 const db = `${__dirname}/dev-data/data/tours-simple.json`;
 
@@ -21,9 +32,13 @@ const tours = JSON.parse(fs.readFileSync(db));
 //callback is called the route handler in express
 
 const getAllTours = (req, res) => {
+  // using our own middleware
+  console.log(req.requestTime);
+
   // formatting using JSend spec
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
