fca4e1559e2dc11b90bc6f9adfc22458df2a2201
joe-nas <jonas@falcken.de>
2023-04-11 15:47:50 +0200

    serving static files/ not on route using express.static()

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -10,6 +10,7 @@ const app = express();
 // Middleware: can modify the incoming request data
 app.use(morgan('dev'));
 app.use(express.json()); // adds the request body to request object, would be undefined otherwise
+app.use(express.static(`${__dirname}/public`)); // if no route is specified sets argument to root folder
 // specify route and router in middleware.
 // Mounting the router, mounting a router on a route
 
