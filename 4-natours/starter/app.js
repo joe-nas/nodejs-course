ef3d0ccae83921f4a42915e9e2938fb9bd3b2989
joe-nas <jonas@falcken.de>
2023-04-11 16:32:18 +0200

    adding and using dotenv package with config.env. Adding config.env to .gitignore. Adding app startup scripts

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -8,7 +8,9 @@ const app = express();
 
 //? 1. Middlewares
 // Middleware: can modify the incoming request data
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json()); // adds the request body to request object, would be undefined otherwise
 app.use(express.static(`${__dirname}/public`)); // if no route is specified sets argument to root folder
 // specify route and router in middleware.
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+//! run app code after we have loaded the environmental varibles
 const app = require('./app');
-console.log(process.env);
 
-const port = 3000;
-
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}....`);
+  console.log(
+    `App running on port ${port} and in ${process.env.NODE_ENV} mode`
+  );
 });
