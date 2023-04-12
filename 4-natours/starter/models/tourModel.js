6bf935d2fd664017b72e531ec325872a69e29b7e
joe-nas <jonas@falcken.de>
2023-04-12 19:35:23 +0200

    playing with mongoose document middleware and pre/post hooks on 'save'

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration!'],
@@ -64,6 +66,27 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+// DOCUMENT MIDDLEWARE
+// pre HOOK runs before save() and create() commands
+tourSchema.pre('save', function (next) {
+  // this points to the document being saved
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function (next) {
+//   // this points to the document being saved
+//   console.log('Will save document...');
+//   next();
+// });
+
+// // post HOOK
+// // has access to the finished document and next
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
