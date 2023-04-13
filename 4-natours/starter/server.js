41ac06877c43c0391026de732f9bef3ffb00e42c
joe-nas <jonas@falcken.de>
2023-04-13 18:07:30 +0200

    catching uncaught exceptions

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('uncaughtException 💥💥💥 shutting down...');
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -26,7 +32,9 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
+  console.log(err);
   console.log('unhandledRejection 💥💥💥 shutting down...');
   server.close(() => process.exit(1));
 });
+
+// console.log(x);
