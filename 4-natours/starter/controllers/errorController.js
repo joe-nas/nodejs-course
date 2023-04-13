c3395a2f16b84907ecadfa497daedea28b510a17
joe-nas <jonas@falcken.de>
2023-04-13 16:10:19 +0200

    refactoring code, handling errors in production/development differently

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -1,11 +1,57 @@
-// by specifying 4 arguments Express knows this is an error handling middleware
+/**
+ *
+ * @param {object} err - Instance of  Error
+ * @param {object} res - Instance of  Response
+ */
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
 
+/**
+ *
+ * @param {object} err - Instance of  Error
+ * @param {object} res - Instance of  Response
+ */
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error, send message to client.
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.log('ERROR 💥💥💥', err);
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
+/**
+ *
+ * @param {object} err - Instance of  Error
+ * @param {object} req - Instance of Request
+ * @param {object} res - Instance of  Response
+ * @param {function} next - next() function
+ */
+// by specifying 4 arguments Express knows this is an error handling middleware
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
