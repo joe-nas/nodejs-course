83921affa40e184f3f768a39d34bc6784fd79e7f
joe-nas <jonas@falcken.de>
2023-04-14 11:43:39 +0200

    implemented a pre save hook/middelware for password hashing using bcrypt

--- 4-natours/starter/models/userModel.js
+++ 4-natours/starter/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -26,7 +27,26 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password!'],
+    validate: {
+      validator: function (pwdC) {
+        // only works on CREATE and SAVE. NOT UPDATE
+        return pwdC === this.password;
+      },
+      message: 'Passwords are not the same! ',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run if password was modified
+  if (!this.isModified('password')) return next();
+
+  // Hash/encrypt the password with a cost of 14
+  this.password = await bcrypt.hash(this.password, 14);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
